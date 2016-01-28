73005cc52ab492f410ebe6292b0327607aac531a
michaelpanciera <michael.panciera.work@gmail.com>
2016-01-28 09:49:19 -0500

    build file and tree edn

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject clj-make "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[instaparse "1.4.1"]
+                 [defun "0.3.0-alapha"]
+                 [org.clojure/clojure "1.7.0"]])
